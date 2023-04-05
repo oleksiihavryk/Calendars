f8f3fd63956a7d97f65544120a9c3a1a6b07a0b1
prser1 <aleksei.gavrik2004@gmail.com>
2023-04-05 16:07:47 +0300

    add bootstrap, favicons  and all core modules.

---
+++ Web/Application/Calendars.Web/src/app/calendars/calendars.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CalendarsRoutingModule } from './calendars-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CalendarsRoutingModule
+  ]
+})
+export class CalendarsModule { }
---
+++ Web/Application/Calendars.Web/src/app/days/days.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DaysRoutingModule } from './days-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DaysRoutingModule
+  ]
+})
+export class DaysModule { }
