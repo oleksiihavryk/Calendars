65bd8bad1f664e9b679554dd46bb616ef37dbccd
prser1 <aleksei.gavrik2004@gmail.com>
2023-04-21 17:29:23 +0300

    Revert "Configure .htaccess and previous commit is useless"
    This reverts commit 6f5502923ae8e9ccce06fc7a76f3b0601af219c5.

--- Web/Application/Calendars.Web/src/app/app.module.ts
+++ Web/Application/Calendars.Web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
@@ -19,7 +20,7 @@ import { FooterModule } from './footer/footer.module';
     TextPagesModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
