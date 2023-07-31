63347fe90a68d91bebd55f50be059360c596da91
oleksiihavryk <oleksii.havryk2004@gmail.com>
2023-07-31 17:27:36 +0300

    Bug is fixed. --Change all 'Login' and 'Logout' names on 'Sign in' and 'Sign out'. --Bug with innapropriate rendering of footer navigation is solved.

--- Web/Application/Calendars.Web/src/app/footer/footer-navigation/footer-navigation.component.ts
+++ Web/Application/Calendars.Web/src/app/footer/footer-navigation/footer-navigation.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthorizeService } from 'src/app/authentication/services/authorize.service';
 
 @Component({
   selector: 'app-footer-navigation',
   templateUrl: './footer-navigation.component.html',
   styleUrls: ['./footer-navigation.component.css']
 })
-export class FooterNavigationComponent { }
+export class FooterNavigationComponent { 
+  constructor(public authorize: AuthorizeService) {}
+}
