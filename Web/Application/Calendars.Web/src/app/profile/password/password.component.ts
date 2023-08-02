3f8c4f006b22460fafa39d1b19f7afc1e7e72a3e
oleksiihavryk <oleksii.havryk2004@gmail.com>
2023-08-02 19:27:35 +0300

    Add changing password feature to system. Fix bug with email with 'text-break' class and input element.

--- Web/Application/Calendars.Web/src/app/profile/password/password.component.ts
+++ Web/Application/Calendars.Web/src/app/profile/password/password.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalService } from 'src/app/shared/services/modal.service';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { User } from 'src/app/shared/domain/user';
+import { AuthorizeService } from 'src/app/authentication/services/authorize.service';
 
 @Component({
   selector: 'app-password',
@@ -7,11 +12,67 @@ import { ModalService } from 'src/app/shared/services/modal.service';
   styleUrls: ['./password.component.css']
 })
 export class PasswordComponent {
-  public featureIsUnavailableModalId: string = 'FeatureIsUnavailableModalId';
+  public changePasswordErrorModalId: string = 'ChangePasswordErrorModalId';
+  
+  public isChangePasswordPanelOpen: boolean = false;
+  public errorMessages: string[] = [];
 
-  constructor(private modal: ModalService) {}
+  public oldPassword: FormControl = new FormControl('', [
+    Validators.required
+  ]);
+  public newPassword: FormControl = new FormControl('', [
+    Validators.required
+  ]);
+  public form: FormGroup = new FormGroup({
+    oldPass: this.oldPassword,
+    newPass: this.newPassword
+  });
 
-  public featureUnavailable() {
-    this.modal.toggleModal(this.featureIsUnavailableModalId);
+  constructor(
+    private modal: ModalService,
+    private userService: UserService,
+    private router: Router,
+    private authorize: AuthorizeService) { }
+
+  public toggleChangePasswordPanel(panel: HTMLElement) {
+    this.isChangePasswordPanelOpen = !this.isChangePasswordPanelOpen;
+    
+    $(panel).stop().slideToggle(300);
+  }
+  public changePasswords(panel: HTMLElement): void {
+    if (this.form.valid === false) 
+      throw new Error('Form cannot be invalid on this stage of password changing.');
+
+    const {id, username, email} = this.authorize.userData;
+
+    if (id === undefined || username === undefined)
+      throw new Error('Incorrect userdata! User id and username cannot be undefined on this stage of password changing.')
+
+    var user = new User(id, username, email, this.oldPassword.value, this.newPassword.value);
+    const obs = this.userService.update(user);
+    obs.subscribe({
+      next: () => {
+        this.router.navigateByUrl('/logout');
+      },
+      error: (err) => {
+        this.errorMessages = err.error.messages;
+        this.modal.toggleModal(this.changePasswordErrorModalId);
+        this.reinitializeForm();
+        this.toggleChangePasswordPanel(panel);
+      },
+    });
+  }
+
+  private reinitializeForm():void {
+    this.oldPassword = new FormControl('', [
+      Validators.required
+    ]);
+    this.newPassword = new FormControl('', [
+      Validators.required
+    ]);
+    this.form = new FormGroup({
+      oldPass: this.oldPassword,
+      newPass: this.newPassword
+    });
   }
 }
